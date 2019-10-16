0a2fac353b6e9db9f3be3e5dcfaaa09afa3af80e
Kevin Bourmier <k.bourmier@refuel.fr>
2019-10-16 11:55:58 +0200

    add custom model

--- src/config/mongoConnection.ts
+++ src/config/mongoConnection.ts
@@ -1,3 +1,5 @@
 import mongoose from 'mongoose';
 
-export default mongoose.connect('mongodb+srv://'+ process.env.MONGO_USER + ':' + process.env.MONGO_PASSWORD + '@' + process.env.MONGO_HOST + '/' + process.env.MONGO_NAME + '?retryWrites=true', { useNewUrlParser: true, useUnifiedTopology: true });
+export default mongoose.connect('mongodb+srv://'+ process.env.MONGO_USER + ':' +
+    process.env.MONGO_PASSWORD + '@' + process.env.MONGO_HOST + '/' + process.env.MONGO_NAME + '?retryWrites=true',
+    { useNewUrlParser: true, useUnifiedTopology: true });
---
+++ src/models/custom.ts
@@ -0,0 +1,20 @@
+import mongoose, {Schema, Document} from 'mongoose';
+import {IProduct} from "./product";
+
+export interface ICustom extends Document{
+    name: string,
+    type: IProduct['id'],
+    price: number,
+    image: String,
+}
+
+const customSchema = new Schema({
+    name: {type: String, required: true, unique: true},
+    type: [{ type: Schema.Types.ObjectId, ref: 'Product'}],
+    price: {type: Number, required: true},
+    image: {type: String, required: true, unique: true},
+
+});
+
+
+export default mongoose.model<ICustom>('Custom', customSchema);
