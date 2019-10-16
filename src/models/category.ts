30f2f6224533b7116d9399a560556bfcaefbe301
Luis Valdez <valdez.jds.16@gmail.com>
2019-10-16 11:08:25 +0200

    add model product

---
+++ src/models/category.ts
@@ -0,0 +1,12 @@
+import mongoose, {Schema, Document} from 'mongoose';
+
+export interface ICategory extends Document{
+    name: string,
+}
+
+const categorySchema = new Schema({
+    name : {type: String, required: true, unique: true}
+});
+
+
+export default mongoose.model<ICategory>('Category', categorySchema);
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,2 +1,4 @@
 
 export {IUser as IUser, default as UserModel} from './user'
+export {IProduct as IProduct, default as ProductModel} from './product'
+export {ICategory as ICategory, default as CategoryModel} from './category'
